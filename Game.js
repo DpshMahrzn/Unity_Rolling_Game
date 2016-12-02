67db4789c7183525a85ce2311584fd5d79457208
DpshMahrzn <jyaps123@gmail.com>
2016-12-02 12:55:29 +0545

    Create Game.js

---
+++ Game.js
@@ -0,0 +1,26 @@
+var isQuit=false;
+
+function OnMouseEnter(){
+	GetComponent.<Renderer>().material.color=Color.red;
+}
+
+function OnMouseExit(){
+	GetComponent.<Renderer>().material.color=Color.white;
+}
+
+function OnMouseUp(){
+	if(isQuit==true)
+	{
+		Application.Quit();
+	}
+	else
+	{
+		Application.LoadLevel(1);
+	}
+}
+
+function Update(){
+	if(Input.GetKey(KeyCode.Escape)){
+		Application.Quit();
+	}
+} 
